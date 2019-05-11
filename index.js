71ce4ae1a7d41fdacd12ac0f453cd5cc37303720
weizheng pan <noone@weizhengdeMacBook-Pro.local>
2019-05-11 17:36:05 +0800

    feat: v2.0 底层改用 wechaty

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-/*!
- * wechat4u.js
- * Copyright(c) 2016-2017 nodeWechat
- * MIT Licensed
- */
+require('babel-register')
+const { Wechaty } = require('wechaty')
+const qrcode = require('qrcode-terminal')
+const schedule = require('node-schedule')
 
-'use strict'
+const bot = Wechaty.instance({ profile: 'no-ane' })
+bot
+  .on('scan', url => {
+    qrcode.generate(url, {
+      small: true
+    })
+  })
+  .on('login',  async user => {
+    console.log(`User ${user} logined`)
+    scheduleJob()
+  })
+  .start()
 
-module.exports = require('./src/wechat')
+
+async function sendRoom (content) {
+  let contact = await bot.Room.find({topic: '名侦探狄仁杰断案奇才'})
+  await contact.say(content)
+}
+
+async function scheduleJob () {
+  /**
+   * 开启定时器叫龟儿刷牙
+   */
+  schedule.scheduleJob('0 30 22 * * *', async () => {
+    /**
+     * 发送文本消息，可以包含emoji(😒)和QQ表情([坏笑])
+     */
+    try {
+      await sendRoom('龟儿刷牙拉')
+    } catch (err) { console.log('error', err) }
+  })
+}
